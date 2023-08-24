1f0f8e353cbe8b7a70d61541863ffd166dddad19
zola-25 <29863888+zola-25@users.noreply.github.com>
2023-08-24 20:56:58 +0100

    more webpack compilation

---
+++ Energy.App.Standalone/src/ts/Files/types.ts
@@ -0,0 +1,71 @@
+﻿import { XYChart } from "@amcharts/amcharts5/xy";
+import { color, Color} from "@amcharts/amcharts5"; 
+export interface ChartReading {
+    dateTicks: number;
+    pencePerKWh: number;
+    cost: number;
+    dailyStandingCharge: number;
+    kWh: number;
+    halfHourlyStandingCharge: number;
+    tariffAppliesFrom: Date;
+    tariffType: string;
+    isForecast: boolean;
+}
+
+export interface ChartDailyForecastReading {
+    dateTicks: number,
+    pencePerKWh: number,
+    cost: number,
+    dailyStandingCharge: number,
+    kWh: number,
+    tariffAppliesFrom: Date
+}
+
+export interface MeterChartProfile {
+    showCost: boolean,
+    globalId: string,
+    profileStart: number,
+    profileEnd: number,
+    mostRecentWeekStart: number,
+    oneMonthInTheFuture: number,
+    latestReading: number,
+    chartReadings: ChartReading[],
+    chartDailyForecastReadings: ChartDailyForecastReading[]
+    highlightStart: number | null,
+    highlightEnd: number | null,
+}
+
+
+
+export interface TemperaturePoint {
+    temperatureCelsiusUnmodified: number;
+    temperatureCelsius: number;
+    dateTicks: number;
+    summary: string;
+}
+
+export interface ChartDetails {
+        chart: XYChart,
+        start: number,
+        end: number,
+        latestReading: number,
+        oneMonthInTheFuture: number,
+        consumptionSeriesIndex: number,
+        costSeriesIndex: number,
+        forecastConsumptionSeriesIndex: number,
+        forecastCostSeriesIndex: number,
+        weatherIconSeriesIndex: number
+    }
+
+export  class ChartDefaults {
+    
+    public static consumptionColor: Color = color(0x594ae2)
+    public static costColor: Color = color(0x272c34)
+    public static costFill: Color = color(0x93969a)
+    public static whiteColor: Color = color(0xffffff)
+    public static blackColor: Color = color(0x000000)
+    public static highlightColor: Color = color(0xFFA726)
+    public static tariffLabelFormat: string = "[bold]£{valueY.formatNumber('#,###.00')}[/]\nTariff: [/]{tariffAppliesFrom.formatDate('MMM dt, yyyy')}[/]\nRate: {pencePerKWh}p/kWh[/]\nStanding Charge (daily): {dailyStandingCharge}p"
+    public static tariffForecastLabelFormat: string = "[bold]Forecast: £{valueY.formatNumber('#,###.00')}[/]\nTariff: [/]{tariffAppliesFrom.formatDate('MMM dt, yyyy')}[/]\nRate: {pencePerKWh}p/kWh[/]\nStanding Charge (daily): {dailyStandingCharge}p"
+
+}
---
+++ Energy.App.Standalone/src/webpack.htmlb.dev.config.js
@@ -0,0 +1,120 @@
+﻿/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import { resolve as _resolve, dirname } from "path";
+import { fileURLToPath } from 'url';
+import * as sass from 'sass'
+import HtmlBundlerPlugin from 'html-bundler-webpack-plugin'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log("__dirname resolves: " + __dirname);
+
+export default {
+    resolve: {
+        alias: {
+            '@fontawesome-css': path.join(__dirname, 'css-precompiled/fontawesome/css'), // => add alias to the font directory
+          },
+      
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlBundlerPlugin({
+            // define a relative or absolute path to entry templates
+            outputPath: _resolve(__dirname, '../wwwroot'),
+            entry: {
+                index: _resolve(__dirname, 'views/index_template.html'),
+
+            },
+            loaderOptions: {
+                sources: [
+                    {
+                        tag: 'meta',
+                        filter: (tag) => { return false }
+                    },
+                    {
+                        tag: 'link',
+                        attributes: ['href'],
+                        filter:  ({ value }) => {
+                            if(value.startsWith('_content') ||
+                                value.startsWith('_framework') ||
+                                value.startsWith('Energy.App.Standalone')){
+                                    return false;
+                                }
+                            return true;
+
+                        }
+                    },
+                    {
+                        tag: 'script',
+                        attributes: ['src'],
+                        filter: ({ value }) => {
+                            if(value.startsWith('_content') ||
+                                value.startsWith('_framework') ||
+                                value.startsWith('Energy.App.Standalone')){
+                                    return false;
+                                }
+                            return true;
+
+                        }
+                    }
+                ]
+            },
+            js: {
+                filename: 'js/[name].[contenthash].js',
+            },
+            css: {
+                filename: 'css/[name].[contenthash].css',
+            }
+            // OR define many templates manually
+        }),
+
+
+    ],
+    mode: 'development',
+    output: {
+        path: _resolve(__dirname, '../wwwroot'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader', options: {
+                            implementation: sass,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2|woff)$/,
+                type: 'asset/resource',
+                include: _resolve(__dirname, 'scss/app/fonts/'),
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
+            {
+                test: /\.(woff2|woff)$/,
+                type: 'asset/resource',
+                include: _resolve(__dirname, 'scss/fontawesome/webfonts/'),
+                generator: {
+                    filename: 'webfonts/[name][ext]',
+                },
+            }
+        ],
+    },
+
+    
+
+};
