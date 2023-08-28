4efaf03ac767f77ec296596c6d25516ce160a58d
zola-25 <29863888+zola-25@users.noreply.github.com>
2023-08-28 05:45:44 +0100

    NugetCreditsbuilder

--- Energy.App.Standalone/src/attributionAndLicensing/NugetLicenseGeneration/NugetCreditsBuilder.js
+++ Energy.App.Standalone/src/attributionAndLicensing/NugetLicenseGeneration/NugetCreditsBuilder.js
@@ -17,18 +17,16 @@ const argv = yargs(hideBin(process.argv))
         type: 'string',
         demandOption: false,
         describe: 'The path to the license info override file, which overrides all license info for a package. Defaults to the script directory/LicenseInfoOverride.json',
-        default: './NugetLicenseInfoOverride.json',
+
     })
     .option('licenseUrlToLicenseTypeOverrideFile', {
         type: 'string',
         demandOption: false,
         describe: 'The path to the license url to license type override file, which overrides the license type for a given license url. Defaults to the script directory/NugetLicenseUrlToLicenseTypeOverride.json',
-        default: './NugetLicenseUrlToLicenseTypeOverride.json',
     }).option('licensePlainTextFolder', {
         type: 'string',
         demandOption: false,
         describe: 'The path to the folder containing the plain text spdx license files. Defaults to the script directory/SpdxLicensePlainTextFiles',
-        default: './SpdxLicensePlainTextFiles',
     }).option('generatedHtmlDocumentPath', {
         type: 'string',
         demandOption: false,
@@ -37,25 +35,31 @@ const argv = yargs(hideBin(process.argv))
         type: 'string',
         demandOption: false,
         describe: 'The path to the temporary license output folder where the license generation will take place. Defaults to the script directory/NugetLicenseTempOutput',
-        default: './NugetLicenseTempOutput',
     }).option('nugetCreditsTemplateFile', {
         type: 'string',
         demandOption: false,
         describe: 'The path to the nuget credits template file. Defaults to the script directory/NugetCreditsPartialTemplate.hbs',
-        default: './NugetCreditsPartialTemplate.hbs'
     }).argv;
 
-const scriptDirectory = path.dirname(process.argv[1]);
-const tempLicenseOutputFolder = path.join(scriptDirectory, argv.tempLicenseOutputFolder);
 
+const scriptDirectory = path.dirname(process.argv[1]);
 console.log(`Script directory: ${scriptDirectory}`);
+
+const licenseInfoOverrideFile = argv.licenseInfoOverrideFile ? argv.licenseInfoOverrideFile : path.join(scriptDirectory, './LicenseInfoOverride.json');
+const licenseUrlToLicenseTypeOverrideFile = argv.licenseUrlToLicenseTypeOverrideFile ? argv.licenseUrlToLicenseTypeOverrideFile : path.join(scriptDirectory, './LicenseUrlToLicenseTypeOverride.json');
+const licensePlainTextFolder = argv.licensePlainTextFolder ? argv.licensePlainTextFolder : path.join(scriptDirectory, './SpdxLicensePlainTextFiles');
+const generatedHtmlDocumentPath = argv.generatedHtmlDocumentPath ? argv.generatedHtmlDocumentPath : path.join(scriptDirectory, './NugetCredits.html');
+const nugetCreditsTemplateFile = argv.nugetCreditsTemplateFile ? argv.nugetCreditsTemplateFile : path.join(scriptDirectory, './NugetCreditsPartial.hbs');
+const tempLicenseOutputFolder = argv.tempLicenseOutputFolder ? argv.tempLicenseOutputFolder : path.join(scriptDirectory, './NugetLicenseTempOutput');
+
+const parentProjectPath = path.resolve(argv.parentProjectPath);
 
 try {
     console.log('Clearing NuGet caches');
     execSync('dotnet nuget locals all --clear');
 
     console.log('Restoring project packages');
-    execSync(`dotnet restore ${argv.parentProjectPath}`);
+    execSync(`dotnet restore ${parentProjectPath}`);
 
     if (fs.existsSync(tempLicenseOutputFolder)) {
         fs.rmSync(tempLicenseOutputFolder, { recursive: true });
@@ -64,43 +68,54 @@ try {
 
     const tempPackageLicenseJsonFileOutput = path.join(tempLicenseOutputFolder, 'Licenses.json');
 
-    const licenseInfoOverride = JSON.parse(fs.readFileSync(argv.licenseInfoOverrideFile));
+    const licenseInfoOverrideParsed = JSON.parse(fs.readFileSync(licenseInfoOverrideFile), 'utf8');
     const tempLicenseOverridePackageNamesFile = path.join(tempLicenseOutputFolder, 'LicenseOverridePackageNames.json');
 
-    fs.writeFileSync(tempLicenseOverridePackageNamesFile, JSON.stringify(licenseInfoOverride.PackageName));
+    fs.writeFileSync(tempLicenseOverridePackageNamesFile, JSON.stringify(licenseInfoOverrideParsed.map(c => c.PackageName)), 'utf8');
 
-    execSync(`dotnet-project-licenses -i ${argv.parentProjectPath} -u -t -o -j  --use-project-assets-json  --outfile ${tempPackageLicenseJsonFileOutput} --packages-filter ${tempLicenseOverridePackageNamesFile} --manual-package-information ${argv.licenseInfoOverrideFile} --licenseurl-to-license-mappings ${argv.licenseUrlToLicenseTypeOverrideFile}`, { cwd: tempLicenseOutputFolder });
+    execSync(`dotnet-project-licenses -i ${parentProjectPath} -u -t -o -j  --use-project-assets-json  --outfile ${tempPackageLicenseJsonFileOutput} --packages-filter ${tempLicenseOverridePackageNamesFile} --manual-package-information ${licenseInfoOverrideFile} --licenseurl-to-license-mappings ${licenseUrlToLicenseTypeOverrideFile}`, { cwd: tempLicenseOutputFolder });
 
-    const packageInfos = JSON.parse(fs.readFileSync(tempPackageLicenseJsonFileOutput));
+    const parsedPackageInfos = JSON.parse(fs.readFileSync(tempPackageLicenseJsonFileOutput, 'utf8'));
 
-    const packageInfosWithLicenseText = [];
-    for (const packageInfo of packageInfos) {
+    const encodedPackageInfosWithLicenseText = [];
+    for (const parsedPackageInfo of parsedPackageInfos) {
 
-        const licenseTextFile = path.join(argv.licensePlainTextFolder, `${packageInfo.LicenseType}.txt`);
+        const licenseTextFilePath = path.join(licensePlainTextFolder, `${parsedPackageInfo.LicenseType}.txt`);
 
-        if (!fs.existsSync(licenseTextFile)) {
-            throw new Error(`License text file not found: ${licenseTextFile}`);
+        if (!fs.existsSync(licenseTextFilePath)) {
+            throw new Error(`License text file not found: ${licenseTextFilePath}`);
         }
-        const licensePlainText = fs.readFileSync(licenseTextFile, 'utf8');
-        const licensePlainTextEncoded = he.encode(licensePlainText);
+        const licensePlainText = fs.readFileSync(licenseTextFilePath, 'utf8');
+        const licensePlainTextEncoded = he.encode(licensePlainText, { strict: true });
 
-        delete packageInfo.LicenseUrl;
+        const AuthorsJoined = parsedPackageInfo.Authors && parsedPackageInfo.Authors.length > 0 ? "Authors: " + parsedPackageInfo.Authors.join(', ') : '';
+        const AuthorsEncoded = he.encode(AuthorsJoined, { strict: true });
 
-        const packageInfoWithLicenseText = { ...packageInfo, LicenseTextEncodedToInsert: licensePlainTextEncoded };
+        const LicenseTypeEncoded = he.encode(parsedPackageInfo.LicenseType, { strict: true });
 
-        packageInfosWithLicenseText.push(packageInfoWithLicenseText);
+        const CopyrightEncoded = he.encode(parsedPackageInfo.Copyright, { strict: true });
+
+        const PackageNameEncoded = he.encode(parsedPackageInfo.PackageName, { strict: true });
+
+        const encodedPackageInfo = { PackageName: PackageNameEncoded, Copyright: CopyrightEncoded, LicenseType: LicenseTypeEncoded, Authors: AuthorsEncoded, LicenseTextEncodedToInsert: licensePlainTextEncoded };
+
+        encodedPackageInfosWithLicenseText.push(encodedPackageInfo);
     }
 
-    const templateText = fs.readFileSync(argv.nugetCreditsTemplateFile, 'utf8');
+    const templateText = fs.readFileSync(nugetCreditsTemplateFile, 'utf8');
 
     const template = Handlebars.compile(templateText, { preventIndent: true, strict: true, noEscape: true });
-    const htmlPartial = template({ packageInfos: packageInfosWithLicenseText });
+    const htmlRendered = template({ packageInfos: encodedPackageInfosWithLicenseText });
 
-    const html = DOMPurify.sanitize(htmlPartial, { USE_PROFILES: { html: true } });
+    const htmlSanitized = DOMPurify.sanitize(htmlRendered, { USE_PROFILES: { html: true } });
 
-    console.log(DOMPurify.removed.map((x) => JSON.stringify(x)).join(", ") + " tags were removed from the html");
+    console.log('DOMPurify removed %d elements.', DOMPurify.removed.length)
 
-    fs.writeFileSync(argv.generatedHtmlDocumentPath, html);
+    if (DOMPurify.removed.length > 0) {
+        console.log('Elements removed:  %s', JSON.stringify(DOMPurify.removed));
+    }
+
+    fs.writeFileSync(generatedHtmlDocumentPath, htmlSanitized, 'utf8');
 
 
 } catch (error) {
@@ -111,8 +126,10 @@ try {
 
 } finally {
 
+    console.log('Clearing temporary license output folder');
     if (fs.existsSync(tempLicenseOutputFolder)) {
         fs.rmSync(tempLicenseOutputFolder, { recursive: true });
     }
 
+    console.log('Completed')
 }
