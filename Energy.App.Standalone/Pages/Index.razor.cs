1bd0390372d1896065632242476ec919b261f0bd
zola-25 <29863888+zola-25@users.noreply.github.com>
2023-11-12 04:16:51 +0000

    Welcome Screen auto show

--- Energy.App.Standalone/Pages/Index.razor.cs
+++ Energy.App.Standalone/Pages/Index.razor.cs
@@ -1,8 +1,19 @@
+using Energy.App.Standalone.Features.Setup.TermsAndConditions;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+
 namespace Energy.App.Standalone.Pages;
 
 public partial class Index
 {
+    [Inject] IState<TermsAndConditionsState> TermsAndConditionsState { get; set; }
 
-
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        bool termsAccepted = TermsAndConditionsState.Value.WelcomeScreenSeenAndDismissed;
+        bool defaultOpenWizard = !termsAccepted;
+        OpenWizard = OpenWizard || defaultOpenWizard;
+    }
 
 }
